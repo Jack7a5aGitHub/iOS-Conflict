7986fe18fc8e1bceb459f6bf4bb55692d4dbace9
Jack Wong <jack7a5a1993@icloud.com>
2018-08-31 16:39:24 +0900

    Base created

---
+++ iOS-Conflict/iOS-Conflict/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  iOS-Conflict
+//
+//  Created by Jack Wong on 2018/08/31.
+//  Copyright © 2018 Jack Wong. All rights reserved.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+    
+    private let number = 12345
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("abc")
+        print("Hello World")
+        print(123)
+        print(number)
+    }
+}
+
